95edc7c525d68d26dd86b54ac0b8c35dbbf35c45
Walton Lee <walton.lee@tufts.edu>
2016-08-18 23:55:14 +0800

    panicking

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
   acts_as_token_authenticatable 
   acts_as_followable
   acts_as_follower
+  validates :name, presence: true, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
