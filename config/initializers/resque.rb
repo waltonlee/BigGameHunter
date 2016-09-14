3799479bdc22067e437437feb6c8251814a2bbca
Walton Lee <walton.lee@tufts.edu>
2016-09-13 22:44:15 -0400

    ay

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,4 +1,5 @@
 if Rails.env == 'production'
+  require 'redis'
   ENV["REDISTOGO_URL"] ||= "redis://redistogo:50c726316702440b953078266d49d608@viperfish.redistogo.com:11866/"
 
   uri = URI.parse(ENV["REDISTOGO_URL"])
