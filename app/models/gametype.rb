b1fa16e73148b31722b5fe2d8dff04c77c1f20cc
waltonlee <walton.lee@tufts.edu>
2016-09-19 00:44:30 -0400

    something

--- app/models/gametype.rb
+++ app/models/gametype.rb
@@ -1,5 +1,5 @@
 class Gametype < ActiveRecord::Base
-  has_many :games #dependent: destroy? maybe?
+  has_many :games, :dependent => :delete_all
   has_and_belongs_to_many :users
   validates :name, presence: true, length: {maximum: 50}
   validates :image, presence: true
