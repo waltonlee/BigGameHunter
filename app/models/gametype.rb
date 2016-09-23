28bbcf02cd19ffeccdd0bf942b70c04812f788b7
waltonlee <walton.lee@tufts.edu>
2016-09-22 21:41:05 -0400

    cleaning up

--- app/models/gametype.rb
+++ app/models/gametype.rb
@@ -3,5 +3,6 @@ class Gametype < ActiveRecord::Base
   has_and_belongs_to_many :users
   validates :name, presence: true, length: {maximum: 50}
   validates :image, presence: true
+  default_scope {order('name ASC')}
 
 end
