339fe3412d14668e2220a8f492f3e6e716f17fbc
waltonlee <walton.lee@tufts.edu>
2016-10-13 14:17:11 -0400

    hah

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   :user_name => 'bghmailer@gmail.com',
-  :password => '66767467',
+  :password => 'tuftsemen',
   :domain => 'gmail.com',
   :address => 'smtp.gmail.com',
   :port => 587,
