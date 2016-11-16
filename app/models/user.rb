32f70d5d4172cb1d56f68ccbdc39961d5b7fcee7
waltonlee <walton.lee@tufts.edu>
2016-11-16 14:52:25 -0500

    login, logout, register implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   has_many :games, dependent: :destroy
   has_and_belongs_to_many :attended_events, class_name: 'Game'
   has_and_belongs_to_many :gametypes
-  acts_as_token_authenticatable 
+  acts_as_token_authenticatable
   acts_as_followable
   acts_as_follower
   validates :name, presence: true, uniqueness: true
@@ -12,5 +12,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+   def skip_confirmation!
+     self.confirmed_at = Time.now
+   end
 
 end
