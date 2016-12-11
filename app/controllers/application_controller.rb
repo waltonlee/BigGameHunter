ee8044fc7472b2960453914174c0df194337f0e7
waltonlee <walton.lee@tufts.edu>
2016-12-11 13:26:50 -0500

    something something backend passwordS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery :except => [:new, :create] # with: :exception
+  protect_from_forgery with: :exception
 
-  before_action :authenticate_user!
   acts_as_token_authentication_handler_for User, except: [:index, :show]
+  #before_action :authenticate_user
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
   protected
